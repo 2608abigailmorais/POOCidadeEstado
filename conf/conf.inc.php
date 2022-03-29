3fe52514a21bb99d9cefea829ea20e103e6c7cab
2608abigailmorais <85850400+2608abigailmorais@users.noreply.github.com>
2022-03-29 20:22:36 -0300

    Add files via upload

---
+++ conf/conf.inc.php
@@ -0,0 +1,14 @@
+<?php
+    // Banco de Dados
+    define('HOST', 'localhost');  
+    define('DBNAME', 'aula');    
+    define('USER', 'root');  
+    define('PASSWORD', 'admin');
+
+    define('DRIVER', 'mysql'); 
+    define('CHARSET', 'utf8');
+
+    // Geral da Aplicação
+    define('NOME_DO_PROJETO','');
+    define('DESCRICAO_DO_PROJETO','POO Estado e Cidade');
+?>
