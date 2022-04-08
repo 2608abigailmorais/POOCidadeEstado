36485f141f309f8e4803e289699b334aa05180f5
2608abigailmorais <85850400+2608abigailmorais@users.noreply.github.com>
2022-04-08 16:02:05 -0300

    Add files via upload

--- classes/cidade.class.php
+++ classes/cidade.class.php
@@ -1,40 +1,40 @@
 <?php
     class Cidade{
-        private $cidId;
-        private $cidNome;
-        private $estadoId;
+        private $id_cidade;
+        private $nome_cid;
+        private $id_estado;
         
         public function __construct($id, $cid, $idest){ 
-            $this->cidId = $id;
-            $this->cidNome = $cid;
-            $this->estadoId = $idest;
+            $this->id_cidade = $id;
+            $this->nome_cid = $cid;
+            $this->id_estado = $idest;
         }
 
         public function __toString(){
-            $str = "ID da Cidade: ".$this->cidId.
-            "<br>Nome da Cidade: ".$this->cidNome.
-            "<br>Estado: ".$this->estadoId;
+            $str = "ID da Cidade: ".$this->id_cidade.
+            "<br>Nome da Cidade: ".$this->nome_cid.
+            "<br>Estado: ".$this->id_estado;
             return $str;
         }
 
         public function inserir(){
             
             $pdo = Conexao::getInstance();
-            $stmt = $pdo->prepare('INSERT INTO cidade (cidNome, estadoId) VALUES(:cidNome, :estadoId)');
-            $stmt->bindParam(':cidNome', $this->cidNome, PDO::PARAM_STR);
-            $stmt->bindParam(':estadoId', $this->estadoId, PDO::PARAM_STR);
+            $stmt = $pdo->prepare('INSERT INTO cidade (nome_cid, id_estado) VALUES(:nome_cid, :id_estado)');
+            $stmt->bindParam(':nome_cid', $this->nome_cid, PDO::PARAM_STR);
+            $stmt->bindParam(':id_estado', $this->id_estado, PDO::PARAM_STR);
     
             return $stmt->execute();
             
         }
 
-        public function editar($cidId){
+        public function editar($id_cidade){
             
             $pdo = Conexao::getInstance();
-        $stmt = $pdo->prepare('UPDATE cidade SET cidNome = :cidNome, estadoId = :estadoId WHERE cidId = :cidId');
-        $stmt->bindParam(':cidId', $cidId, PDO::PARAM_INT);
-        $stmt->bindParam(':cidNome', $this->cidNome, PDO::PARAM_STR);
-        $stmt->bindParam(':estadoId', $this->estadoId, PDO::PARAM_STR);
+        $stmt = $pdo->prepare('UPDATE cidade SET nome_cid = :nome_cid, id_estado = :id_estado WHERE id_cidade = :id_cidade');
+        $stmt->bindParam(':id_cidade', $id_cidade, PDO::PARAM_INT);
+        $stmt->bindParam(':nome_cid', $this->nome_cid, PDO::PARAM_STR);
+        $stmt->bindParam(':id_estado', $this->id_estado, PDO::PARAM_STR);
     
 
     
@@ -43,10 +43,10 @@
         }
 
 
-        function excluir($cidId){
+        function excluir($id_cidade){
             $pdo = Conexao::getInstance();
-            $stmt = $pdo ->prepare('DELETE FROM cidade WHERE cidId = :cidId');
-            $stmt->bindParam(':cidId', $cidId);
+            $stmt = $pdo ->prepare('DELETE FROM cidade WHERE id_cidade = :id_cidade');
+            $stmt->bindParam(':id_cidade', $id_cidade);
             
             return $stmt->execute();
         }
